ecb23b5dbea2691bb4b9334afc61e7bac9a0c44a
Abhishek Sharma <abhisheks@berkeley.edu>
2023-04-10 21:10:02 -0700

    Added basic text, first milestone for react native app

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.tinyLogo} source={require("./assets/Icon-192.png")} />
+      <Text style={styles.textPadded}>Welcome to FitFlow!</Text>
+      <Text style={styles.textPadded}>Click the buttons below to customize your workout.</Text>
+      <Text style={styles.textPadded}>If you're already at the gym, click "Start Workout"</Text>
+      <Exercise name="Bench Press" number="1"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: 80,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    paddingBottom: 30,
+  },
+  textPadded: {
+    paddingTop: 25,
   },
 });
+
+const Exercise = props => {
+  return (
+    <View style={styles.textPadded}>
+      <Text>Exercise #{props.number}: {props.name}</Text>
+    </View>
+  );
+};
