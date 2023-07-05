6546241235564d3876a0cca0fe03947516d32c93
Abhishek Sharma <abhisheks@berkeley.edu>
2023-07-04 23:37:32 -0700

    uncommented firebase request

--- App.js
+++ App.js
@@ -85,11 +85,11 @@ function App() {
       // Check if an hour has passed since last update
       if (true || !lastUpdated || now - lastUpdated >= 60 * 60 * 1000) {
         // Fetch data from firebase
-        // const url = `https://lift-logger-alpha-default-rtdb.firebaseio.com/exercises.json`;
-        // const response = await fetch(url);
-        // const data = await response.text(); // Extract response as text
-        // const jsonData = JSON.parse(data); // Parse JSON string
-        // console.log("p1", jsonData[0]);
+        const url = `https://lift-logger-alpha-default-rtdb.firebaseio.com/exercises.json`;
+        const response = await fetch(url);
+        const data = await response.text(); // Extract response as text
+        const jsonData = JSON.parse(data); // Parse JSON string
+        console.log("p1", jsonData[0]);
 
         // Create table in local db and insert data
         createExerciseTable()
